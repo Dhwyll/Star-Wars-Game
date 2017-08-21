d542f8536da379cca0679bac68923d7a3bfb87f9
Brian Evans <dhwyll@gmail.com>
2017-08-21 12:05:28 -0700

    Filled out comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,5 @@
 // RPG Javascript
 
-// Set up objects for each of the characters
-	// Image
-	// Name
-	// Health Points
-	// Attack Power
-	// Counter Attack Power
-	// Chosen as Player
-	// Chosen as Enemy
-
 $(document).ready(function(){
 
 	var charObiWan = {
@@ -62,40 +53,40 @@ $(document).ready(function(){
 	var chosenEnemy;
 
 	function setupCharacters() {
-		$("#characterChoiceSection").css("display", "none");
-		$("#enemyChoiceSection").css("display", "none");
-		for (i = 0; i < playerCharArray.length; i++) {
-			$("#charName-"+i).html(playerCharArray[i].charName);
-			$("#charImage-"+i).attr("src", playerCharArray[i].charImage);
-			$("#charHealth-"+i).html(playerCharArray[i].charHealth);
+		$("#characterChoiceSection").css("display", "none");					// Hide the character choice section
+		$("#enemyChoiceSection").css("display", "none");						// Hide the enemy choice section
+		for (i = 0; i < playerCharArray.length; i++) {							// For the length of the player character array
+			$("#charName-"+i).html(playerCharArray[i].charName);				// Set the player name
+			$("#charImage-"+i).attr("src", playerCharArray[i].charImage);		// Set the player image
+			$("#charHealth-"+i).html(playerCharArray[i].charHealth);			// Set the player health
 		}
-		for (i = 0; i < 3; i++) {
-			$("#enemy-"+i).css("display", "none");
-		$("#battleDetailsSection").css("display", "none");
+		for (i = 0; i < 3; i++) {												// For the total number of characters
+			$("#enemy-"+i).css("display", "none");								// Hide the enemy block
+		$("#battleDetailsSection").css("display", "none");						// Hide the Battle Details Section
 		}
 	}
 	
 	function setupEnemies() {
-		enemyCharArray = [];
-		for (i = 0; i < playerCharArray.length; i++) {
-			if (!playerCharArray[i].amIChosen) {
-				enemyCharArray.push(playerCharArray[i]);
+		enemyCharArray = [];													// Clear the enemy array
+		for (i = 0; i < playerCharArray.length; i++) {							// For the length of the player character array
+			if (!playerCharArray[i].amIChosen) {								// If the player character is not chosen
+				enemyCharArray.push(playerCharArray[i]);						// Then push that character into the enemy array
 			}
 		}
 		displayEnemies();														// run display enemy function
 	}
 	
 	function displayEnemies() {
-		$("#enemyList").css("display", "block");
-		for (i = 0; i < 3; i++) {
-			$("#enemy-"+i).css("display", "none");
+		$("#enemyList").css("display", "block");										// Show enemy list
+		for (i = 0; i < 3; i++) {														// For all characters
+			$("#enemy-"+i).css("display", "none");										// Hide the character
 		}
-		for (i = 0; i < enemyCharArray.length; i++) {
-			if (!enemyCharArray[i].amIChosen) {
-				$("#enemyName-"+i).html(enemyCharArray[i].charName);
-				$("#enemyImage-"+i).attr("src", enemyCharArray[i].charImage);
-				$("#enemyHealth-"+i).html(enemyCharArray[i].charHealth);
-				$("#enemy-"+i).css("display", "block");
+		for (i = 0; i < enemyCharArray.length; i++) {									// For the length of the number of enemies left
+			if (!enemyCharArray[i].amIChosen) {											// If the enemy has not been chosen
+				$("#enemyName-"+i).html(enemyCharArray[i].charName);					// Set the enemy's name
+				$("#enemyImage-"+i).attr("src", enemyCharArray[i].charImage);			// Set the enemy's image
+				$("#enemyHealth-"+i).html(enemyCharArray[i].charHealth);				// Set the enemy's health
+				$("#enemy-"+i).css("display", "block");									// Show the enemies in the list
 			}
 		}
 	}
@@ -127,51 +118,51 @@ $(document).ready(function(){
 	});
 	
 	$("#attack").on("click",function(){
-		if (inBattle) {
-			$("#battleDetailsSection").css("display", "block");
-			$("#resetGame").css("display", "none");
-			if ((chosenPlayer.charHealth > 0) && (chosenEnemy.charHealth > 0)) {
-				$("#playerAttack").html("You attacked " + chosenEnemy.charName + " for " + chosenPlayer.charCurrentAttack + " damage.");
-				chosenEnemy.charHealth = chosenEnemy.charHealth - chosenPlayer.charCurrentAttack;
-				chosenPlayer.charCurrentAttack = chosenPlayer.charCurrentAttack + chosenPlayer.charBaseAttack;
-				$("#enemyChoiceHealth").html(chosenEnemy.charHealth);
-				if(chosenEnemy.charHealth > 0) {
-					chosenPlayer.charHealth = chosenPlayer.charHealth - chosenEnemy.charBaseAttack;
-					$("#enemyAttack").html(chosenEnemy.charName + " attacked you back for " + chosenEnemy.charBaseAttack  + " damage.");
-					$("#characterChoiceHealth").html(chosenPlayer.charHealth);
-					if(chosenPlayer.charHealth <= 0) {
-						$("#playerAttack").html("You have been defeated. . .GAME OVER");
-						$("#enemyAttack").html("");
-						$("#resetGame").css("display", "block");
+		if (inBattle) {																			// If in battle
+			$("#battleDetailsSection").css("display", "block");									// Show the Battle Detail Section
+			$("#resetGame").css("display", "none");												// Hide the Reset Button
+			if ((chosenPlayer.charHealth > 0) && (chosenEnemy.charHealth > 0)) {				// If the player and enemy are still alive
+				$("#playerAttack").html("You attacked " + chosenEnemy.charName + " for " + chosenPlayer.charCurrentAttack + " damage.");	// Display player's attack
+				chosenEnemy.charHealth = chosenEnemy.charHealth - chosenPlayer.charCurrentAttack;											// Attack enemy
+				chosenPlayer.charCurrentAttack = chosenPlayer.charCurrentAttack + chosenPlayer.charBaseAttack;								// Increase player's attack
+				$("#enemyChoiceHealth").html(chosenEnemy.charHealth);															// Display enemy's reduced health
+				if(chosenEnemy.charHealth > 0) {																				// If enemy is still alive...
+					chosenPlayer.charHealth = chosenPlayer.charHealth - chosenEnemy.charBaseAttack;								// Attack player
+					$("#enemyAttack").html(chosenEnemy.charName + " attacked you back for " + chosenEnemy.charBaseAttack  + " damage.");	// Display enemy's attack
+					$("#characterChoiceHealth").html(chosenPlayer.charHealth);													// Display player's reduced health
+					if(chosenPlayer.charHealth <= 0) {																			// If player dead...
+						$("#playerAttack").html("You have been defeated. . .GAME OVER");										// Display Game Over
+						$("#enemyAttack").html("");																				// Clear enemy attack notice
+						$("#resetGame").css("display", "block");																// Display Reset button.
 					}
 				}
-					else if(enemyCharArray.length > 0) {
-						$("#enemyChoiceSection").css("display", "none");
-						$("#playerAttack").html("You have defeated " + chosenEnemy.charName + ".  You can choose to fight another enemy.");
-						$("#enemyAttack").html("");
-						inBattle = false;
+					else if(enemyCharArray.length > 0) {																		// Else if out of enemies
+						$("#enemyChoiceSection").css("display", "none");														// Hide chosen enemy
+						$("#playerAttack").html("You have defeated " + chosenEnemy.charName + ".  You can choose to fight another enemy.");	// Tell select new enemy
+						$("#enemyAttack").html("");																				// Clear enemy attack notice
+						inBattle = false;																						// No longer in battle
 					}
-						else {
-							$("#playerAttack").html("You won!!! GAME OVER!!!");
-							$("#enemyChoiceSection").css("display","none");
-							$("#enemyAttack").html("");
-							inBattle = false;
-							$("#resetGame").css("display", "block");
+						else {																									// Else, player won
+							$("#playerAttack").html("You won!!! GAME OVER!!!");													// Display Game Won
+							$("#enemyChoiceSection").css("display","none");														// Hide enemy choice
+							$("#enemyAttack").html("");																			// Clear enemy attack notice
+							inBattle = false;																					// No longer in battle
+							$("#resetGame").css("display", "block");															// Display Reset Game button
 						}
 			}
 		}
 	});
 
 	$("#resetGame").on("click",function(){
-		inBattle = false;
-		playerCharArray = [charObiWan, charLuke, charPalpatine, charMaul];
-		$("#characterList").css("display", "block");
-		$("#characterChoiceSection").css("display", "none");
-		$("#enemyChoiceSection").css("display", "none");
-		$("#enemyList").css("display", "none");
-		$("#playerAttack").html("");
-		$("#enemyAttack").html("");
-		$("#resetGame").css("display", "none");
+		inBattle = false;																				// No longer in battle
+		playerCharArray = [charObiWan, charLuke, charPalpatine, charMaul];								// Reset Player Character Array
+		$("#characterList").css("display", "block");													// Show Character List
+		$("#characterChoiceSection").css("display", "none");											// Hide player choice
+		$("#enemyChoiceSection").css("display", "none");												// Hide enemy choice
+		$("#enemyList").css("display", "none");															// Hide enemy array
+		$("#playerAttack").html("");																	// Clear player attack notice
+		$("#enemyAttack").html("");																		// Clear enemy attack notice
+		$("#resetGame").css("display", "none");															// Hide Reset Game button
 	});
 
 
